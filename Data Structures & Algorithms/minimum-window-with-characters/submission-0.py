8180da936fb5d5f51a99ce984ecf9ae2220f64cc
Aayush Rajendra Sawant <144760904+aayushNortheastern1905@users.noreply.github.com>
2026-06-18 19:00:42 -0400

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = Counter(t);
+        window = {}
+        have , need_count = 0 , len(need)
+        res, res_len = [-1, -1], float('inf')
+        left = 0
+
+        for right, char in enumerate(s):
+            window[char] = window.get(char,0) + 1
+            
+            if char in need and window[char] == need[char]:
+                have +=1
+
+                while have == need_count:
+
+                    if(right - left + 1) < res_len:
+                        res = [left, right]
+                        res_len = right - left + 1
+
+
+                    window[s[left]] -= 1
+                    if s[left] in need and window[s[left]] < need[s[left]]:
+                        have -= 1
+
+                    left += 1
+
+        l, r = res
+        return s[l:r + 1] if res_len != float('inf') else ""
